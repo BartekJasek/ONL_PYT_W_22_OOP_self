330546bdf330ed86deb1feaf7c43be535cd89382
BartekJasek <bartek.jasek5@gmail.com>
2022-11-05 14:00:52 +0100

    zadanie2

--- 01_Dzien_01/04_Wyrazenie_regularne/02_Zadanie_2/exercise_2.py
+++ 01_Dzien_01/04_Wyrazenie_regularne/02_Zadanie_2/exercise_2.py
@@ -4,3 +4,11 @@ import re
 with open('text.txt', 'r') as fin:
     text_to_search = fin.read()
     #  Tutaj umieść swoje rozwiązanie!
+    word_autor = re.findall(r'[Aa]utor', text_to_search)
+    print(word_autor)
+    number_sequence = re.findall(r'\d+', text_to_search)
+    print(number_sequence)
+    dot = re.findall(r'[a-zA-Z]*[.]', text_to_search)
+    print(dot)
+    word_polski = re.findall(r'[Pp]olski', text_to_search)
+    print(word_polski)

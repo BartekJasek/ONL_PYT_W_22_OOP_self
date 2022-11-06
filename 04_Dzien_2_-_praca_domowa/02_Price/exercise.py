3d3d1423facdf0a74a8d3856a9418981a2eee058
BartekJasek <bartek.jasek5@gmail.com>
2022-11-06 11:24:13 +0100

    zadanieprice

---
+++ 04_Dzien_2_-_praca_domowa/02_Price/exercise.py
@@ -0,0 +1,44 @@
+class Price23Vat:
+    def __init__(self, brutto):
+        self._brutto = brutto
+        self._netto = self._brutto / 1.23
+        self._tax = self._brutto - self._netto
+
+    def get_brutto(self):
+        return self._brutto
+
+    def get_netto(self):
+        return self._netto
+
+    def get_tax(self):
+        return self._tax
+
+    def set_brutto(self, value):
+        new_brutto = value
+        self.__init__(new_brutto)
+
+    def set_netto(self, value):
+        new_brutto = value * 0.77
+        self.__init__(new_brutto)
+
+    def set_tax(self, value):
+        new_brutto = value * 0.23
+        self.__init__(new_brutto)
+
+
+s = Price23Vat(10)
+s.set_brutto(100)
+print(s.get_brutto())
+print(s.get_netto())
+print(s.get_tax())
+
+
+s.set_netto(100)
+print(s.get_brutto())
+print(s.get_netto())
+print(s.get_tax())
+
+s.set_tax(100)
+print(s.get_brutto())
+print(s.get_netto())
+print(s.get_tax())
